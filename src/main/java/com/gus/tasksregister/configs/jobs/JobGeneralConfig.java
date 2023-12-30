27e0061ba20db7e9a5f3e4e8521e3f8bd5e8bf3d
Gustavo Avila <gustavogavila01@gmail.com>
2023-12-30 15:54:52 -0300

    jobLauncher configurado

---
+++ src/main/java/com/gus/tasksregister/configs/jobs/JobGeneralConfig.java
@@ -0,0 +1,21 @@
+package com.gus.tasksregister.configs.jobs;
+
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class JobGeneralConfig {
+
+    @Bean
+    public JobLauncher jobLauncher(JobRepository jobRepository) throws Exception {
+        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+        jobLauncher.setJobRepository(jobRepository);
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
+    }
+}
--- src/main/java/com/gus/tasksregister/services/TaskBatchService.java
+++ src/main/java/com/gus/tasksregister/services/TaskBatchService.java
@@ -1,13 +1,31 @@
 package com.gus.tasksregister.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class TaskBatchService {
 
-    public void startProcess(MultipartFile file) {
-        // TODO Configurar o launcher do job
+    private final JobLauncher jobLauncher;
+    private final Job registerNewTasksJob;
+
+    public void startProcess(MultipartFile file) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        // TODO Passar arquivo recebido para o job
+        jobLauncher.run(registerNewTasksJob, new JobParametersBuilder()
+                .addString("jobUUID", UUID.randomUUID().toString())
+                .toJobParameters()
+        );
         // TODO Configurar job e passar arquivo para ele (inicialmente salvando no sistema de arquivos) e utilizando o FlatFileItemReader
         // TODO Passar arquivo para o job diretamente (sem salvar no sistema de arquivos) e utilizando um ItemReader customizado
     }
